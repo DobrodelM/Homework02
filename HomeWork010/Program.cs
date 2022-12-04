481781e8b98176388f2f61b398dd90a861c83357
filin <filin@dobrodel@yandex.ru>
2022-12-04 18:14:22 +0300

    add Homework3

---
+++ HomeWork010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int GetRandomNumber(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+
+int GetSecondDigitFromNumber(int number)
+{
+    int secondDigit = 0;
+    secondDigit = (number % 100) / 10;
+    return secondDigit;
+}
+
+int randomNumber = GetRandomNumber(100, 999);
+int secondDigit = GetSecondDigitFromNumber(randomNumber);
+Console.WriteLine($"В числе {randomNumber} второй цифрой являтся цифра {secondDigit}");
+
